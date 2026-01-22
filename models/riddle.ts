e42f33ab34a836eaeec17cb8c7a0287fac90731d
Chen Peleg <chenpeleg@gmail.com>
2026-01-22 19:11:35 +0200

    wip

--- models/riddle.ts
+++ models/riddle.ts
@@ -8,10 +8,13 @@ export interface RiddleSource {
 export interface Riddle {
   id: string;
   text: string;
+  textHe?: string;
   solution?: string;
+  solutionHe?: string;
   categories: string[];
   image?: string;
   clue?: string;
+  clueHe?: string;
   difficulty?: RiddleDifficulty;
   source: RiddleSource;
 }
